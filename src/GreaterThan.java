e3413ff57f616f0a0105b5a64cb79a7446365e71
Rimpaldeep <rimgill007@gmail.com>
2023-02-04 10:01:47 -0500

    first commit

---
+++ src/GreaterThan.java
@@ -0,0 +1,17 @@
+public class GreaterThan {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 17;
+        int c = 10;
+        if (a>b && a>c) {
+            System.out.println("The maximum number is:");
+            System.out.println("a = " + a);
+        } else if (b>a && b>c) {
+            System.out.println("The maximum number is:");
+            System.out.println("b = " + b);
+        } else {
+            System.out.println("The maximum number is:");
+            System.out.println("c = " + c);
+        }
+    }
+}
